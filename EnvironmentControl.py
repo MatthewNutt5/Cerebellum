7670a5a1ae5217053c611f673559516d4cc84f7d
Matt N <matthewcnutt@gmail.com>
2026-02-10 11:57:51 -0600

    added EnvironmentControl library; incomplete

---
+++ EnvironmentControl.py
@@ -0,0 +1,9 @@
+"""
+EnvironmentControl.py
+This file contains the EnvironmentControl library, which is used to send
+commands and receive data from the devices of the test environment. The primary
+function is runTest, which executes the test specified by a TestSettings object
+on the test environment specified by an EnvironmentConfig object.
+"""
+
+def runTest(config, settings):
