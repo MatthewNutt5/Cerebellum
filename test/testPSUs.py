be446d7426f1f44a530ca189f7d0f2fe892c3735
Matt N <matthewcnutt@gmail.com>
2026-02-20 12:22:39 -0600

    added JSON read/write to PSU test

--- test/testPSUs.py
+++ test/testPSUs.py
@@ -6,20 +6,30 @@ from Cerebellum.EnvironmentConfig import EnvironmentConfig, PSUConfig
 from Cerebellum.TestSettings import TestSettings, PSUSettings, Criterion
 from Cerebellum.EnvironmentControl import runTest
 
+readJSON = False
+
 config = EnvironmentConfig()
-config.PSUConfigList.append(PSUConfig())
-config.PSUConfigList[0].protocol = "IP"
-config.PSUConfigList[0].IP = "192.168.0.40"
-config.PSUConfigList[0].interface = "SCPI"
-config.PSUConfigList[0].channel = 0
+if readJSON:
+    config.readJSON("config.json")
+else:
+    config.PSUConfigList.append(PSUConfig())
+    config.PSUConfigList[0].protocol = "IP"
+    config.PSUConfigList[0].IP = "192.168.0.40"
+    config.PSUConfigList[0].interface = "SCPI"
+    config.PSUConfigList[0].channel = 0
+    config.writeJSON("config.json")
 
 settings = TestSettings()
-settings.PSUSettingsList.append(PSUSettings())
-settings.PSUSettingsList[0].voltage = 1.0
-settings.PSUSettingsList[0].current = 0.005
-settings.criteriaList.append(Criterion("PSUCurrent"))
-settings.criteriaList[0].PSUidx = 0
-settings.criteriaList[0].ineq = "<"
-settings.criteriaList[0].PSUCurrent = 0.1
+if readJSON:
+    settings.readJSON("settings.json")
+else:
+    settings.PSUSettingsList.append(PSUSettings())
+    settings.PSUSettingsList[0].voltage = 1.0
+    settings.PSUSettingsList[0].current = 0.015
+    settings.criteriaList.append(Criterion("PSUCurrent"))
+    settings.criteriaList[0].PSUidx = 0
+    settings.criteriaList[0].ineq = "<"
+    settings.criteriaList[0].PSUCurrent = 0.1
+    settings.writeJSON("settings.json")
 
 runTest(config, settings)
