e068a819b0dd8ec77e7b21b68e41329e7631c1d6
Matt N <matthewcnutt@gmail.com>
2026-03-05 19:06:47 -0600

    reconfigured test script

--- test/testPSU.py
+++ test/testPSU.py
@@ -3,7 +3,7 @@ sys.path.append('../')
 sys.path.append('../Cerebellum/')
 
 from Cerebellum.EnvironmentConfig import EnvironmentConfig, PSUConfig
-from Cerebellum.TestSettings import TestSettings, PSUSettings, Criterion
+from Cerebellum.TestSettings import TestSettings, SetPSUEvent, EvalPSUVoltageEvent, EvalPSUCurrentEvent
 from Cerebellum.EnvironmentControl import runTest
 
 readJSON = False
@@ -13,25 +13,26 @@ if readJSON:
     config.readJSON("config.json")
 else:
     config.PSUConfigList.append(PSUConfig())
+    config.PSUConfigList[0].displayName = "Low Voltage Power Supply"
+    config.PSUConfigList[0].interface = "SCPI"
     # config.PSUConfigList[0].protocol = "IP"
     # config.PSUConfigList[0].IP = "192.168.0.40"
     config.PSUConfigList[0].protocol = "Serial"
     config.PSUConfigList[0].COM = "COM7"
-    config.PSUConfigList[0].interface = "SCPI"
-    config.PSUConfigList[0].channel = 0
     config.writeJSON("config.json")
 
 settings = TestSettings()
 if readJSON:
     settings.readJSON("settings.json")
 else:
-    settings.PSUSettingsList.append(PSUSettings())
-    settings.PSUSettingsList[0].voltage = 1.0
-    settings.PSUSettingsList[0].current = 0.015
-    settings.criteriaList.append(Criterion("PSUCurrent"))
-    settings.criteriaList[0].PSUidx = 0
-    settings.criteriaList[0].ineq = "<"
-    settings.criteriaList[0].PSUCurrent = 0.1
+    settings.PSUSettingsList.append(SetPSUEvent())
+    settings.PSUSettingsList[0].PSUidx = 0
+    settings.PSUSettingsList[0].channel = 0
+    settings.PSUSettingsList[0].voltage = 9.0
+    settings.PSUSettingsList[0].current = 6.0
+    settings.eventList.append(EvalPSUCurrentEvent())
+    settings.eventList[0].PSUidx = 0
+    settings.eventList[0].channel = 0
     settings.writeJSON("settings.json")
 
 runTest(config, settings)
